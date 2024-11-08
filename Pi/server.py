79c31a3a9244d37a45dcb56b88748cfbed7f99f3
Nyx Smith <nsmith@sflscientific.com>
2024-11-08 18:03:04 -0500

    add stub fns for test

--- Pi/server.py
+++ Pi/server.py
@@ -9,5 +9,18 @@ def handle_post():
     response = {"message": "Data received", "received_data": data}
     return jsonify(response)
 
+@app.route("/servo_position", methods=["GET"])
+def get_servo_position():
+    # TODO link into servo.py
+    response = {"message": "Data received to get servo position"}
+    return jsonify(response)
+
+@app.route("/desired_temperature", methods=["GET","POST"])
+def desired_temperature():
+    # TODO: Implement this function for post and get
+    response = {"message": "Data received to get servo position"}
+    return jsonify(response)
+
+
 if __name__ == "__main__":
     app.run(port=9955,host='0.0.0.0')
